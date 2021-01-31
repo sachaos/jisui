61f82451dc223deb6c82e3e4dd18f59ce9a11a6f
Takumasa Sakao <sakataku7@gmail.com>
2021-01-31 16:17:48 +0900

    Convert PDF

--- go.mod
+++ go.mod
@@ -5,6 +5,10 @@ go 1.15
 require (
 	cloud.google.com/go v0.75.0
 	cloud.google.com/go/storage v1.12.0
+	github.com/golang/protobuf v1.4.3
+	github.com/phpdave11/gofpdf v1.4.2
+	github.com/signintech/gopdf v0.9.13
 	github.com/tj/go-spin v1.1.0
+	google.golang.org/api v0.36.0
 	google.golang.org/genproto v0.0.0-20210126160654-44e461bb6506
 )
--- main.go
+++ main.go
@@ -4,10 +4,15 @@ import (
 	"cloud.google.com/go/storage"
 	vision "cloud.google.com/go/vision/apiv1"
 	"flag"
+	"github.com/golang/protobuf/jsonpb"
+	"github.com/phpdave11/gofpdf"
+	"github.com/phpdave11/gofpdf/contrib/gofpdi"
 	"github.com/tj/go-spin"
+	"google.golang.org/api/iterator"
 	visionpb "google.golang.org/genproto/googleapis/cloud/vision/v1"
 	"io"
 	"strconv"
+	"strings"
 	"time"
 
 	"context"
@@ -25,10 +30,20 @@ func main() {
 	}
 }
 
+func collectAnnotations(responses []*visionpb.AnnotateFileResponse) map[int]*visionpb.TextAnnotation {
+	annotations := map[int]*visionpb.TextAnnotation{}
+	for _, response := range responses {
+		for _, res := range response.Responses {
+			annotations[int(res.Context.PageNumber)] = res.FullTextAnnotation
+		}
+	}
+	return annotations
+}
+
 func run() error {
 	flag.StringVar(&bucket, "bucket", "", "GCS bucket")
 	flag.Parse()
-	filename := flag.Arg(1)
+	filename := flag.Arg(0)
 
 	if filename == "" {
 		return fmt.Errorf("invalid argument: filename must specified")
@@ -43,28 +58,49 @@ func run() error {
 	if err != nil {
 		return err
 	}
+	defer file.Close()
 
 	endOfGCS := waitingMessage("waiting for GCS")
-	name, err := uploadPDF(ctx, file)
+	bucket, path, err := uploadPDF(ctx, file)
 	if err != nil {
 		return err
 	}
 	endOfGCS()
 
 	endOfOCR := waitingMessage("waiting for OCR")
-	err = ocrPDF(ctx, name)
+	err = ocrPDF(ctx, bucket, path, bucket, path+"/result")
 	if err != nil {
 		return err
 	}
 	endOfOCR()
 
+	endOfDL := waitingMessage("waiting for Downloading result")
+	responses, err := downloadResponse(ctx, bucket, path+"/result")
+	if err != nil {
+		return err
+	}
+	endOfDL()
+
+	file2, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer file2.Close()
+
+	endOfConvert := waitingMessage("annotating")
+	err = integrateWithPDF(file2, collectAnnotations(responses))
+	if err != nil {
+		return err
+	}
+	endOfConvert()
+
 	return nil
 }
 
-func uploadPDF(ctx context.Context, r io.Reader) (string, error) {
+func uploadPDF(ctx context.Context, r io.Reader) (string, string, error) {
 	client, err := storage.NewClient(ctx)
 	if err != nil {
-		return "", err
+		return "", "", err
 	}
 
 	name := strconv.FormatInt(time.Now().UnixNano(), 10)
@@ -75,13 +111,13 @@ func uploadPDF(ctx context.Context, r io.Reader) (string, error) {
 
 	_, err = io.Copy(writer, r)
 	if err != nil {
-		return "", err
+		return "", "", err
 	}
 
-	return "gs://" + object.BucketName() + "/" + object.ObjectName(), nil
+	return object.BucketName(), object.ObjectName(), nil
 }
 
-func ocrPDF(ctx context.Context, path string) error {
+func ocrPDF(ctx context.Context, srcBucket, srcPath, dstBucket, dstPath string) error {
 	client, err := vision.NewImageAnnotatorClient(ctx)
 	if err != nil {
 		return err
@@ -96,12 +132,12 @@ func ocrPDF(ctx context.Context, path string) error {
 					},
 				},
 				InputConfig: &visionpb.InputConfig{
-					GcsSource: &visionpb.GcsSource{Uri: path},
+					GcsSource: &visionpb.GcsSource{Uri: fmt.Sprintf("gs://%s/%s", srcBucket, srcPath)},
 					MimeType:  "application/pdf",
 				},
 				OutputConfig: &visionpb.OutputConfig{
 					GcsDestination: &visionpb.GcsDestination{
-						Uri: path,
+						Uri: fmt.Sprintf("gs://%s/%s", dstBucket, dstPath),
 					},
 				},
 			},
@@ -113,27 +149,68 @@ func ocrPDF(ctx context.Context, path string) error {
 		return err
 	}
 
-	resp, err := operation.Wait(ctx)
+	_, err = operation.Wait(ctx)
 	if err != nil {
 		return err
 	}
 
-	fmt.Printf("%v", resp)
-
 	return nil
 }
 
+func downloadResponse(ctx context.Context, bucket, prefix string) ([]*visionpb.AnnotateFileResponse, error) {
+	var responses []*visionpb.AnnotateFileResponse
+
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	it := client.Bucket(bucket).Objects(ctx, &storage.Query{
+		Prefix: prefix,
+	})
+
+	for {
+		attrs, err := it.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+
+		object := client.Bucket(bucket).Object(attrs.Name)
+		reader, err := object.NewReader(ctx)
+		if err != nil {
+			return nil, err
+		}
+		defer reader.Close()
+
+		var data visionpb.AnnotateFileResponse
+		m := jsonpb.Unmarshaler{}
+		err = m.Unmarshal(reader, &data)
+		if err != nil {
+			return nil, err
+		}
+
+		responses = append(responses, &data)
+	}
+	
+	return responses, err
+}
+
 func waitingMessage(message string) func() {
+	var msg string
+	msg = message[:]
 	done := make(chan struct{})
 	go func() {
 		s := spin.New()
 		for {
 			select {
 			case <-done:
-				fmt.Printf("[done]\n\n")
+				fmt.Printf("\r  %s [done]\n", msg)
 				return
 			default:
-				fmt.Printf("\r%s %s", s.Next(), message)
+				fmt.Printf("\r%s %s", s.Next(), msg)
 				time.Sleep(100 * time.Millisecond)
 			}
 		}
@@ -142,4 +219,114 @@ func waitingMessage(message string) func() {
 	return func() {
 		close(done)
 	}
+}
+
+func integrateWithPDF(pdfr io.ReadSeeker, annotation map[int]*visionpb.TextAnnotation) error {
+	pdf := gofpdf.New("P", "pt", "A4", "")
+
+	imp := gofpdi.NewImporter()
+	tmpl := imp.ImportPageFromStream(pdf, &pdfr, 1, "/MediaBox")
+	sizes := imp.GetPageSizes()
+	nrPages := len(imp.GetPageSizes())
+
+	pdf.AddUTF8Font("ipa", "", "./ipaexg.ttf")
+
+	for i := 1; i <= nrPages; i++ {
+		w := sizes[i]["/MediaBox"]["w"]
+		h := sizes[i]["/MediaBox"]["h"]
+		anno := annotation[i]
+		pdf.AddPageFormat("P", gofpdf.SizeType{
+			Wd: w,
+			Ht: h,
+		})
+
+		for _, page := range anno.Pages {
+			for _, block := range page.Blocks {
+				for _, paragraph := range block.Paragraphs {
+					for _, word := range paragraph.Words {
+						minX, minY, _, maxY := extract(word.BoundingBox.NormalizedVertices)
+						pdf.SetFillColor(200, 700, 220)
+						//pdf.Rect(w * minX, h * minY, (maxX - minX) * w, (maxY - minY) * h, "F")
+
+						pdf.SetFont("ipa", "", (maxY - minY) * h)
+						pdf.Text(w * minX, h * minY + (maxY - minY) * h, collectWords(word))
+					}
+				}
+			}
+		}
+
+		if i > 1 {
+			tmpl = imp.ImportPageFromStream(pdf, &pdfr, i, "/MediaBox")
+		}
+		imp.UseImportedTemplate(pdf, tmpl, 0, 0, w, h)
+	}
+
+	err := pdf.OutputFileAndClose("result.pdf")
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func extract(vs []*visionpb.NormalizedVertex) (float64, float64, float64, float64) {
+	minX := vs[0].X
+	minY := vs[0].Y
+	maxX := float32(0)
+	maxY := float32(0)
+
+	for _, v := range vs {
+		if v.X < minX {
+			minX = v.X
+		}
+
+		if v.Y < minY {
+			minY = v.Y
+		}
+
+		if v.X > maxX {
+			maxX = v.X
+		}
+
+		if v.Y > maxY {
+			maxY = v.Y
+		}
+	}
+
+	return float64(minX), float64(minY), float64(maxX), float64(maxY)
+}
+
+func collectWords(word *visionpb.Word) string {
+	b := strings.Builder{}
+	for _, symbol := range word.Symbols {
+		b.WriteString(symbol.Text)
+	}
+
+	return b.String()
+}
+
+func collectWordsParagraph(paragraph *visionpb.Paragraph) []string {
+	words := make([]string, len(paragraph.Words))
+	for i, word := range paragraph.Words {
+		b := strings.Builder{}
+		for _, symbol := range word.Symbols {
+			b.WriteString(symbol.Text)
+		}
+		words[i] = b.String()
+	}
+
+	return words
+}
+
+func collectWordsBlock(block *visionpb.Block) string {
+	b := strings.Builder{}
+	for _, paragraph := range block.Paragraphs {
+		for _, word := range paragraph.Words {
+			for _, symbol := range word.Symbols {
+				b.WriteString(symbol.Text)
+			}
+		}
+	}
+
+	return b.String()
 }
